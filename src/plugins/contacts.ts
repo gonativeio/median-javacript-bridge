eb351af1eccbcb0c4611258da6d88e7c9cefee9a
Kevin <87081548+kevzbldai@users.noreply.github.com>
2024-02-01 21:57:30 +0800

    Add pickContact command to contacts plugin (#10)

--- src/plugins/contacts.ts
+++ src/plugins/contacts.ts
@@ -1,44 +1,50 @@
-import { CallbackParams } from '../types';
+import { CallbackData, CallbackParams } from '../types';
 import { addCommandCallback } from '../utils';
 
-type ContactsGetData = {
-  success: boolean;
-  contacts: {
-    birthday?: string;
-    namePrefix?: string;
-    givenName?: string;
-    middleName?: string;
-    familyName?: string;
-    previousFamilyName?: string;
-    nameSuffix?: string;
-    nickname?: string;
-    phoneticGivenName?: string;
-    phoneticMiddleName?: string;
-    phoneticFamilyName?: string;
-    organizationName?: string;
-    departmentName?: string;
-    jobTitle?: string;
-    note?: string;
-    phoneNumbers?: { label: string; phoneNumber: string }[];
-    emailAddresses?: { label: string; emailAddress: string }[];
-    postalAddresses?: {
-      label: string;
-      street: string;
-      city: string;
-      state: string;
-      region: string;
-      postalCode: string;
-      country: string;
-      isoCountryCode: string;
-      subAdministrativeArea: string;
-      subLocality: string;
-    }[];
-    companyName?: string;
-    companyTitle?: string;
+type ContactItem = {
+  birthday?: string;
+  namePrefix?: string;
+  givenName?: string;
+  middleName?: string;
+  familyName?: string;
+  previousFamilyName?: string;
+  nameSuffix?: string;
+  nickname?: string;
+  phoneticGivenName?: string;
+  phoneticMiddleName?: string;
+  phoneticFamilyName?: string;
+  organizationName?: string;
+  departmentName?: string;
+  jobTitle?: string;
+  note?: string;
+  phoneNumbers?: { label: string; phoneNumber: string }[];
+  emailAddresses?: { label: string; emailAddress: string }[];
+  postalAddresses?: {
+    label: string;
+    street: string;
+    city: string;
+    state: string;
+    region: string;
+    postalCode: string;
+    country: string;
+    isoCountryCode: string;
+    subAdministrativeArea: string;
+    subLocality: string;
   }[];
+  companyName?: string;
+  companyTitle?: string;
+};
+
+type ContactsGetData = CallbackData & {
+  contacts?: ContactItem[];
 };
 
 type ContactsPermissionData = { status: 'granted' | 'denied' | 'restricted' | 'notDetermined' };
+
+type ContactsPickData = CallbackData & {
+  contact?: ContactItem;
+  contacts?: ContactItem[];
+};
 
 const contacts = {
   getAll: function (params: CallbackParams<ContactsGetData>) {
@@ -46,6 +52,9 @@ const contacts = {
   },
   getPermissionStatus: function (params: CallbackParams<ContactsPermissionData>) {
     return addCommandCallback<ContactsPermissionData>('median://contacts/getPermissionStatus', params);
+  },
+  pickContact: function (params: CallbackParams<ContactsPickData> & { multiple?: boolean }) {
+    return addCommandCallback<ContactsPickData>('median://contacts/pickContact', params);
   },
 };
 
