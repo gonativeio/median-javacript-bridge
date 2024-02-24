10a90589636c4515f5cccdd5d543dda2cc518517
Hunaid Hassan <hhunaid@gmail.com>
2024-02-24 14:14:22 +0500

    Introudce UMD using rollup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'umd',
+    name: 'Median'
+  },
+  plugins: [typescript(), terser()],
+};
