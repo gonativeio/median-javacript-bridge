27e741ad419df3855a7163959e65e4155efeefba
kevz <kevz@bld.ai>
2024-05-03 10:28:09 +0800

    Add context menu methods

--- src/commands/general.ts
+++ src/commands/general.ts
@@ -32,6 +32,15 @@ const connectivity = {
   },
 };
 
+const contextMenu = {
+  setEnabled: function (enabled: boolean) {
+    addCommand('median://contextMenu/setEnabled', { enabled });
+  },
+  setLinkActions: function (actions: string[]) {
+    addCommand('median://contextMenu/setLinkActions', { actions });
+  },
+};
+
 export type DeviceInfo = {
   platform: 'ios' | 'android';
   appId: string;
@@ -319,6 +328,7 @@ export {
   clipboard,
   config,
   connectivity,
+  contextMenu,
   deviceInfo,
   internalExternal,
   keyboard,
