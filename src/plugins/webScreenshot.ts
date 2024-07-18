ee6a69cefbb0b1b2a4b2714fb4153b169ac534ca
kevz <kevz@bld.ai>
2024-07-18 13:12:41 +0800

    Add webscreenshot capture methods

--- src/plugins/webScreenshot.ts
+++ src/plugins/webScreenshot.ts
@@ -1,4 +1,4 @@
-import { addCommand } from '../utils';
+import { addCommand, addCommandCallback } from '../utils';
 
 interface HtmlCanvasOptions {
   useCORS: boolean;
@@ -27,6 +27,31 @@ const webScreenshot = {
     window.html2canvas(element, { useCORS: true, scale: 1.25 }).then((canvas) => {
       const dataUrl = canvas.toDataURL();
       addCommand('median://webScreenshot/shareElement', { dataUrl, ...params });
+    });
+  },
+  captureScreen: async function () {
+    const result = await addCommandCallback('median://webScreenshot/captureScreen');
+    const binaryString = atob(result.data);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return new Blob([bytes], { type: 'image/png' });
+  },
+  captureElement: async function (element: HTMLElement) {
+    if (!window.html2canvas) {
+      return;
+    }
+    const canvas = await window.html2canvas(element, { useCORS: true, scale: 1.25 });
+    return new Promise<Blob>((resolve, reject) => {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          resolve(blob);
+        } else {
+          reject(new Error('Failed to convert to blob.'));
+        }
+      });
     });
   },
 };
